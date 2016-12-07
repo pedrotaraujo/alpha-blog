b45ca7a169e4c8725bdd7dbb862e7bc47647b745
Pedro Teixeira <pedrotaraujo@gmail.com>
2016-12-07 23:02:31 +0000

    prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
-
+	
+	before_action :set_article, only: [:edit,:update, :show, :destroy]
+	
 	def index
 	
 	@articles = Article.all
@@ -12,11 +14,11 @@ class ArticlesController < ApplicationController
 	end
 	
 	def show
-		@article = Article.find(params[:id])
+		
 	end
 	
 	def destroy
-		@article = Article.find(params[:id])
+	
 		@article.destroy
 		flash[:notice] = "articles successfully deleted"
 		redirect_to articles_path
@@ -35,11 +37,11 @@ class ArticlesController < ApplicationController
 	end
 	
 	def edit
-		@article = Article.find(params[:id])
+	
 	end
 	
 	def update
-		@article = Article.find(params[:id])
+		
 		if @article.update(article_params)
 			
 			flash[:notice] = "Article successfully updated"
@@ -49,6 +51,14 @@ class ArticlesController < ApplicationController
 			render 'edit'
 		end
 	end
+	
+	private
+	def set_article
+		
+		@article = Article.find(params[:id])
+	
+	end
+	
 	
 	private 
 	def article_params
