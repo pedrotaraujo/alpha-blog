e328847ca653f7d8227b8cdad3a5408eb6174a84
Pedro Teixeira <pedrotaraujo@gmail.com>
2016-12-07 22:10:37 +0000

    Partials and linking

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,12 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 	
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		flash[:notice] = "articles successfully deleted"
+		redirect_to articles_path
+	end
 	
 	
 	def create
