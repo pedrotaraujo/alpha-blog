2d76f6037a37e5ee7951b3e93405740be7dfdcba
Pedro Teixeira <pedrotaraujo@gmail.com>
2016-12-07 21:00:21 +0000

    Edit and links on the app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,10 @@
 class ArticlesController < ApplicationController
 
+	def index
+	
+	@articles = Article.all
+	
+	end
 	def new 
 		
 		@article = Article.new
@@ -10,6 +15,8 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 	
+	
+	
 	def create
 		
 		@article = Article.new(article_params)
@@ -19,6 +26,22 @@ class ArticlesController < ApplicationController
 		else
 			render 'new'
 		end	
+	end
+	
+	def edit
+		@article = Article.find(params[:id])
+	end
+	
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			
+			flash[:notice] = "Article successfully updated"
+			redirect_to article_path(@article)
+		
+		else
+			render 'edit'
+		end
 	end
 	
 	private 
